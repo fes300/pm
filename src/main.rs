28c92aa530076280c0a29b0a7b955421ff1ca0c0
fes300 <federicosordillo@gmail.com>
2024-05-20 09:29:58 +0200

    feat: make installation generic

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use clap::{Parser, Subcommand};
 use colorize::AnsiColor;
 use home::home_dir;
 use include_dir::include_dir;
-use inquire::{InquireError, Select};
-use std::fs;
+use inquire::{InquireError, Select, Text};
+use regex::Regex;
+use std::fs::{self, metadata};
 use std::io::{BufRead, BufReader};
 use std::process::{Command, Stdio};
 
@@ -29,34 +30,80 @@ fn main() {
     }
 }
 
-fn flakes_destination_path()-> String { prepend_home(".pm/flakes") }
+fn flakes_destination_path() -> String {
+    prepend_home(".pm/flakes")
+}
 
 fn setup() {
     println!("\n");
-    
+
+    create_pmrc();
+
+    create_flakes_dir();
+
+    let success_log = format!("setup completed");
+
+    println!("\n{}", success_log.bold().b_green());
+}
+
+fn create_pmrc() {
+    let gen_log = format!("generating .pmrc...");
+    println!("{}", gen_log.italic().black());
+
+    let prompt_result: Result<String, String> = Text::new("where do you keep your projects?")
+        .prompt()
+        .map_err(|e| e.to_string())
+        .and_then(|v| {
+            let exploded_path = v.replace("~", home_dir().unwrap().to_str().unwrap());
+            return metadata(exploded_path.clone())
+                .map_err(|e| e.to_string())
+                .and_then(|p| {
+                    if p.is_dir() {
+                        return Ok(exploded_path.clone());
+                    }
+                    return Err("path is not a directory".to_string());
+                });
+        });
+
+    match prompt_result {
+        Ok(r) => {
+            match fs::write(prepend_home(".pmrc"), format!("project_dir={}", r)) {
+                Ok(_) => {
+                    // nothing to do here
+                }
+                Err(e) => {
+                    println!("error writing .pmrc: {}", e)
+                }
+            };
+        }
+        Err(e) => {
+            println!("error writing .pmrc: {}", e);
+            println!("bye! 👋");
+        }
+    };
+}
+
+fn create_flakes_dir() {
     include_dir!("./src/flakes").dirs().for_each(|d| {
         let flake_dir = d.path().file_name().unwrap().to_str().unwrap();
 
-        let gen_log =format!("generating {} flake...", flake_dir);
+        let gen_log = format!("generating {} flake...", flake_dir);
         println!("{}", gen_log.italic().black());
 
-        let flake_content = d.get_file(format!("{}/flake.nix", flake_dir))
+        let flake_content = d
+            .get_file(format!("{}/flake.nix", flake_dir))
             .unwrap()
             .contents_utf8()
             .unwrap();
 
-        let flake_destination_path = format!(
-            "{}/{}",
-            flakes_destination_path(),
-            flake_dir
-        );
+        let flake_destination_path = format!("{}/{}", flakes_destination_path(), flake_dir);
 
         fs::create_dir_all(flake_destination_path.clone()).unwrap();
 
-        match fs::write(format!(
-            "{}/flake.nix",
-            flake_destination_path.clone()
-        ), flake_content) {
+        match fs::write(
+            format!("{}/flake.nix", flake_destination_path.clone()),
+            flake_content,
+        ) {
             Ok(_) => {
                 // println!("created {} flake", flake_dir)
             }
@@ -65,18 +112,24 @@ fn setup() {
             }
         };
     });
-
-    let success_log = format!("setup completed");
-    println!("\n{}", success_log.bold().b_green());
 }
 
 fn boot_project() {
-    let known_projects = vec!["notificationservice", "billingservice", "frontend", "rustcli", "", "-- other projects --", ""];
+    let known_projects = vec![
+        "notificationservice",
+        "billingservice",
+        "frontend",
+        "rustcli",
+        "",
+        "-- other projects --",
+        "",
+    ];
     let mut suggestions = known_projects.clone();
 
-    let all_projects_path = prepend_home("git");
+    let all_projects_path = get_projects_folder();
+    println!("path {}", all_projects_path);
     let _all_projects = list_files(all_projects_path.as_str());
-    
+
     let all_projects: Vec<&str> = _all_projects
         .iter()
         .filter(|v| !known_projects.contains(&v.as_str()))
@@ -85,16 +138,13 @@ fn boot_project() {
 
     suggestions.extend(all_projects.clone());
 
-    let prompt_result: Result<&str, InquireError> = Select::new(
-        "which project should we boot?",
-        suggestions,
-    )
-    .prompt();
+    let prompt_result: Result<&str, InquireError> =
+        Select::new("which project should we boot?", suggestions).prompt();
 
     match prompt_result {
         Ok(proj) => {
-            if proj == "" || proj ==  "-- other projects --" {
-                return
+            if proj == "" || proj == "-- other projects --" {
+                return;
             }
 
             let boot_log = format!("\nbooting {}...", proj);
@@ -102,7 +152,9 @@ fn boot_project() {
 
             let flake_location = flake_location(proj, known_projects);
 
-            let flake_log = format!("using flake at {}\n", flake_location);
+            set_flake_project(flake_location.clone(), proj);
+
+            let flake_log = format!("using flake at {}\n", flake_location.clone());
             println!("{}", flake_log.italic().black());
 
             let mut cmd = Command::new("nix")
@@ -123,7 +175,7 @@ fn boot_project() {
 
             cmd.wait().unwrap();
 
-            cleanup(proj, flake_location);
+            cleanup(proj, flake_location.clone());
 
             let flake_log = format!("booted {}", proj);
             println!("{}", flake_log.bold().b_green());
@@ -144,27 +196,27 @@ fn list_files(path: &str) -> Vec<String> {
 const DEFAULT_FLAKE: &str = "go_1_22";
 
 fn cleanup(proj: &str, flake_location: String) {
-    if flake_location == default_flake_location() {
-        let flake_path = format!("{}/flake.nix", default_flake_location());
-        let default_flake_content = match fs::read_to_string(flake_path.clone()) {
-            Ok(v) => v,
-            Err(e) => {
-                panic!("could not read flake at {}: {}", flake_path.clone(), e)
-            }
-        };
+    let flake_path = format!("{}/flake.nix", flake_location);
+    let flake_content = match fs::read_to_string(flake_path.clone()) {
+        Ok(v) => v,
+        Err(e) => {
+            panic!("could not read flake at {}: {}", flake_path.clone(), e)
+        }
+    };
 
-        match fs::write(
-            flake_path.clone(),
-            default_flake_content.replace(proj, "__PROJECTNAME__"),
-        ) {
-            Ok(_) => {
-                // nothing to do here
-            }
-            Err(e) => {
-                println!("error updating the standard flake: {}", e)
-            }
-        };
-    }
+    match fs::write(
+        flake_path.clone(),
+        flake_content
+            .replace(proj, "__PROJECTNAME__")
+            .replace(&get_projects_folder(), "__PROJECTDIR__"),
+    ) {
+        Ok(_) => {
+            // nothing to do here
+        }
+        Err(e) => {
+            println!("error updating the standard flake: {}", e)
+        }
+    };
 }
 
 fn default_flake_location() -> String {
@@ -176,18 +228,32 @@ fn flake_location(proj: &str, known_projects: Vec<&str>) -> String {
         return format!("{}/{}", flakes_destination_path(), proj);
     }
 
-    let default_flake_path = format!("{}/flake.nix", default_flake_location());
+    println!(
+        "\n{}\n",
+        "booting with a generic go 1_22 flake"
+            .b_yellow()
+            .bold()
+            .underlined()
+    );
 
-    let default_flake_content = match fs::read_to_string(default_flake_path.clone()) {
+    return default_flake_location();
+}
+
+fn set_flake_project(flake_location: String, proj: &str) {
+    let flake_path = format!("{}/flake.nix", flake_location);
+
+    let default_flake_content = match fs::read_to_string(flake_path.clone()) {
         Ok(v) => v,
         Err(e) => {
-            panic!("could not read flake at {}: {}", default_flake_path.clone(), e)
+            panic!("could not read flake at {}: {}", flake_path.clone(), e)
         }
     };
 
     match fs::write(
-        default_flake_path.clone(),
-        default_flake_content.replace("__PROJECTNAME__", proj),
+        flake_path.clone(),
+        default_flake_content
+            .replace("__PROJECTNAME__", proj)
+            .replace("__PROJECTDIR__", &get_projects_folder()),
     ) {
         Ok(_) => {
             // nothing to do here
@@ -196,15 +262,17 @@ fn flake_location(proj: &str, known_projects: Vec<&str>) -> String {
             println!("error updating the standard flake: {}", e)
         }
     };
-
-    println!(
-        "\n{}\n",
-        "booting with a generic go 1_22 flake".b_yellow().bold().underlined()
-    );
-
-    return default_flake_location();
 }
 
+fn get_projects_folder() -> String {
+    let re = Regex::new(r"project_dir=(.+)$").unwrap();
+    return fs::read(prepend_home(".pmrc"))
+        .map(|c| {
+            let s = String::from_utf8(c).unwrap();
+            return re.captures(&s).unwrap().get(1).unwrap().as_str().to_owned();
+        })
+        .unwrap();
+}
 
 fn prepend_home(s: &str) -> String {
     let r = format!("{}/{}", home_dir().unwrap().to_str().unwrap(), s);
